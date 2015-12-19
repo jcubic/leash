c51a85cea6577d30c656b51b4f46042a419215b5
Jakub Jankiewicz <jcubic@onet.pl>
2015-12-19 16:53:57 +0100

    remove init.js

--- init.js
+++
@@ -1,8 +0,0 @@
-// this is your custom init script, you can create your custom code
-// you can push new commands using:
-//
-// $.leash.then(function(leash) {
-//      leash.commands['command'] = function(cmd, token, term) {
-//          your command
-//      };
-// });
