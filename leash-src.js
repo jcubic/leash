8f3a80853d748fa286a5f52fbe4855b433df8ab0
Jakub Jankiewicz <jcubic@onet.pl>
2016-01-23 16:19:51 +0100

    Update year in copyright note

--- leash-src.js
+++ leash-src.js
@@ -28,7 +28,7 @@ var leash = (function() {
         };
     });
     var copyright = [
-        'Copyright (c) 2013-2014 Jakub Jankiewicz <http://jcubic.pl>',
+        'Copyright (c) 2013-2016 Jakub Jankiewicz <http://jcubic.pl>',
         '',
         'This program is free software: you can redistribute it and/or modify',
         'it under the terms of the GNU General Public License as published by',
