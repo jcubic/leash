1c980cf81ebb134a2b4dbd77b738a2c907652fda
Jakub Jankiewicz <jcubic@onet.pl>
2016-08-13 23:23:42 +0200

    Use only one if

--- lib/download.php
+++ lib/download.php
@@ -2,17 +2,11 @@
 
 require('Service.php');
 $service = new Service('config.json', getcwd() . '/..');
-if ($service->valid_token($_GET['token'])) {
-    if (isset($_GET['filename']) && file_exists($_GET['filename'])) {
-        header('Content-Type: application/octet-stream');
-        header("Content-Transfer-Encoding: Binary");
-        header("Content-disposition: attachment; filename=\"" . basename($_GET['filename']) . "\"");
-        readfile($_GET['filename']); // do the double-download-dance (dirty but worky)
-    } else {
-        echo 'wrong file';
-    }
-} else {
-    echo 'invalid token';
+if ($service->valid_token($_GET['token']) && isset($_GET['filename']) && file_exists($_GET['filename'])) {
+    header('Content-Type: application/octet-stream');
+    header("Content-Transfer-Encoding: Binary");
+    header("Content-disposition: attachment; filename=\"" . basename($_GET['filename']) . "\"");
+    readfile($_GET['filename']); // do the double-download-dance (dirty but worky)
 }
 
 ?>
