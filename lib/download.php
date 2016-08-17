8a1e65dbec2bee9709724fa84df476a071e3c83b
Jakub Jankiewicz <jcubic@onet.pl>
2016-08-17 14:47:08 +0200

    Fix content-length of download file

--- lib/download.php
+++ lib/download.php
@@ -3,10 +3,15 @@
 require('Service.php');
 $service = new Service('config.json', getcwd() . '/..');
 if ($service->valid_token($_GET['token']) && isset($_GET['filename']) && file_exists($_GET['filename'])) {
+    $size = filesize($_GET['filename']);
     header('Content-Type: application/octet-stream');
     header("Content-Transfer-Encoding: Binary");
     header("Content-disposition: attachment; filename=\"" . basename($_GET['filename']) . "\"");
+    header("Content-Length: " . $size);
+    header("Content-Range: 0-" . ($size-1) . "/" . $size);
     readfile($_GET['filename']); // do the double-download-dance (dirty but worky)
+} else {
+    echo 'wrong args';
 }
 
 ?>
