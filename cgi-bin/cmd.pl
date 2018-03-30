1b265389cf31ca08d46c5a87a409a00d88f75f73
Jakub Jankiewicz <jcubic@onet.pl>
2018-03-30 20:50:33 +0200

    fix perl cgi script

--- cgi-bin/cmd.pl
+++ cgi-bin/cmd.pl
@@ -8,8 +8,28 @@
 use strict;
 use warnings;
 use lib qw(.);
+use JSON qw( );
 
-use Leash qw(&valid_token);
+sub valid_token {
+    my $filename = '../config.json';
+
+    my $json_text = do {
+        open(my $json_fh, "<:encoding(UTF-8)", $filename)
+            or die("Can't open \$filename\": $!\n");
+        local $/;
+        <$json_fh>
+    };
+
+    my $json = JSON->new;
+    my $config = $json->decode($json_text);
+
+    for ( @{$config->{sessions}} ) {
+        if ($ENV{'QUERY_STRING'} eq $_->{token}) {
+            return 1;
+        }
+    }
+    return 0;
+}
 
 print "Content-Type: text/plain\r\n\r\n";
 
